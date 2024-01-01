78f4b946dee8d7f818b7783415a46cd792c64053
Alicia Lu <lualicia88@gmail.com>
2023-12-31 21:58:21 -0500

    refactor warping (in progress)

---
+++ wormalign/old_warp.py
@@ -0,0 +1,200 @@
+from euler_gpu.preprocess import initialize
+from euler_gpu.transform import transform_image_3d
+from numpy.typing import NDArray
+from wormalign.utils import (locate_dataset, get_cropped_image, 
+    get_image_T, get_image_CM)
+import deepreg.model.layer as layer
+import json
+import nibabel as nib
+import numpy as np
+import tensorflow as tf
+import torch
+
+with open("resources/registration_problems.json", "r") as f:
+    ALL_PROBLEMS = json.load(f)
+
+with open("resources/center_of_mass.json", "r") as f:
+    CM_DICT = json.load(f)
+
+with open("resources/euler_parameters.json", "r") as f:
+    EULER_PARAMETERS_DICT = json.load(f)
+
+with open(f"resources/problem_to_pairnum.json", "r") as f:
+    PAIR_NUM_DICT = json.load(f)
+
+def warp_channel_image(ddf_directory: str,
+                       dataset_name: str,
+                       registration_problem: str) -> NDArray[np.float_]:
+
+    problem_id = f"{dataset_name}/{registration_problem}"
+    pair_num = PAIR_NUM_DICT[problem_id]
+    #pair_num = get_pair_num(dataset_name, registration_problem)
+    network_outputs_path = f"{ddf_directory}/test/pair_{pair_num}"
+    ddf_nii_path = f"{network_outputs_path}/ddf.nii.gz"
+    ddf_array = nib.load(ddf_nii_path).get_fdata()
+    warped_moving_image_nii_path = \
+            f"{network_outputs_path}/pred_fixed_image.nii.gz"
+    warped_moving_image = nib.load(warped_moving_image_nii_path).get_fdata()
+
+    fixed_image_nii_path = \
+            f"{network_outputs_path}/fixed_image.nii.gz"
+    fixed_image = nib.load(fixed_image_nii_path).get_fdata()
+
+    moving_image_nii_path = \
+            f"{network_outputs_path}/moving_image.nii.gz"
+    moving_image = nib.load(moving_image_nii_path).get_fdata()
+
+    return ddf_array, fixed_image, moving_image, warped_moving_image
+
+
+def warp_image_roi(ddf_directory: str,
+                   dataset_name: str,
+                   registration_problem: str) -> NDArray[np.float_]:
+
+    problem_id = f"{dataset_name}/{registration_problem}"
+    pair_num = PAIR_NUM_DICT[problem_id]
+    #pair_num = get_pair_num(dataset_name, registration_problem)
+    network_outputs_path = f"{ddf_directory}/test/pair_{pair_num}"
+    ddf_nii_path = f"{network_outputs_path}/ddf.nii.gz"
+    ddf_array = nib.load(ddf_nii_path).get_fdata()
+
+    nrrd_images_path = locate_dataset(dataset_name)
+    t_moving, t_fixed = registration_problem.split('to')
+    fixed_image_roi_path = \
+            f"{nrrd_images_path}/img_roi_watershed/{t_fixed}.nrrd"
+    moving_image_roi_path = \
+            f"{nrrd_images_path}/img_roi_watershed/{t_moving}.nrrd"
+
+    resized_fixed_image_roi = resize_image_roi(
+                fixed_image_roi_path,
+                CM_DICT[problem_id][1])
+    resized_moving_image_roi = resize_image_roi(
+                moving_image_roi_path,
+                CM_DICT[problem_id][0])
+    device_name = 0
+    # warp the moving image roi with euler parameters
+    euler_warped_moving_image_roi = euler_transform_image_roi(
+                resized_moving_image_roi,
+                problem_id,
+                device_name)
+    # warp the moving image roi with ddf
+    resized_moving_image_roi_tf = tf.cast(
+                tf.expand_dims(euler_warped_moving_image_roi,
+                axis=0), dtype=tf.float32
+    )
+    warping = layer.Warping(fixed_image_size=resized_moving_image_roi_tf.shape[1:4],
+                            interpolation="nearest")
+    warped_moving_image_roi_tf = warping(inputs=[ddf_array,
+                resized_moving_image_roi_tf])
+    warped_moving_image_roi = warped_moving_image_roi_tf.numpy()[0]
+
+    return resized_fixed_image_roi, resized_moving_image_roi, euler_warped_moving_image_roi, warped_moving_image_roi
+
+'''
+def get_pair_num(dataset_name: str, registration_problem: str):
+
+    problems = ALL_PROBLEMS["test"][dataset_name]
+
+    return problems.index(registration_problem)
+'''
+
+def resize_image_roi(image_roi_path: str, image_CM: list[int]):
+
+    image_roi_T = get_image_T(image_roi_path)
+    resized_image_roi = get_cropped_image(
+        image_roi_T, image_CM, (208, 96, 56), -1).astype(np.float32)
+
+    return resized_image_roi
+
+
+def euler_transform_image_roi(moving_image_roi: NDArray[np.int_],
+                problem_id: str,
+                device_name: int) -> NDArray[np.float_]:
+
+    device = torch.device(f"cuda:{device_name}")
+    x_dim, y_dim, z_dim = moving_image_roi.shape
+
+    _memory_dict_xy = initialize(
+            np.zeros((x_dim, y_dim)).astype(np.float32),
+            np.zeros((x_dim, y_dim)).astype(np.float32),
+            np.zeros(z_dim),
+            np.zeros(z_dim),
+            np.zeros(z_dim),
+            z_dim,
+            device
+    )
+    _memory_dict_xz = initialize(
+            np.zeros((x_dim, z_dim)).astype(np.float32),
+            np.zeros((x_dim, z_dim)).astype(np.float32),
+            np.zeros(y_dim),
+            np.zeros(y_dim),
+            np.zeros(y_dim),
+            y_dim,
+            device
+    )
+    _memory_dict_yz = initialize(
+            np.zeros((y_dim, z_dim)).astype(np.float32),
+            np.zeros((y_dim, z_dim)).astype(np.float32),
+            np.zeros(x_dim),
+            np.zeros(x_dim),
+            np.zeros(x_dim),
+            x_dim,
+            device
+    )
+    best_transformation_xy = torch.tensor(
+                EULER_PARAMETERS_DICT[problem_id]["xy"]).to(device)
+    transformed_moving_image_roi_xyz = transform_image_3d(
+                moving_image_roi,
+                _memory_dict_xy,
+                best_transformation_xy,
+                device
+    )
+    best_transformation_xz = torch.tensor(
+                EULER_PARAMETERS_DICT[problem_id]["xz"]).to(device)
+    transformed_moving_image_roi_xzy = transform_image_3d(
+                np.transpose(transformed_moving_image_roi_xyz, (0, 2, 1)),
+                _memory_dict_xz,
+                best_transformation_xz,
+                device
+    )
+    best_transformation_yz = torch.tensor(
+                EULER_PARAMETERS_DICT[problem_id]["yz"]).to(device)
+    transformed_moving_image_roi_yzx = transform_image_3d(
+                np.transpose(transformed_moving_image_roi_xzy, (2, 1, 0)),
+                _memory_dict_yz,
+                best_transformation_yz,
+                device
+    )
+    dz = EULER_PARAMETERS_DICT[problem_id]["dz"]
+    warped_moving_image_roi = np.transpose(transformed_moving_image_roi_yzx, (2, 0, 1))
+
+    final_moving_image_roi_xyz = np.full(
+            warped_moving_image_roi.shape, 0)
+    # translate along the z-axis
+    if dz < 0:
+        final_moving_image_roi_xyz[:, :, :dz] = \
+            warped_moving_image_roi[:, :, -dz:]
+    elif dz > 0:
+        final_moving_image_roi_xyz[:, :, dz:] = \
+            warped_moving_image_roi[:, :, :-dz]
+    elif dz == 0:
+        final_moving_image_roi_xyz = warped_moving_image_roi
+
+    return final_moving_image_roi_xyz
+
+
+if __name__ == "__main__":
+
+    base = "/home/alicia/data_personal/regnet_ckpt/logs_predict"
+    ckpt = "euler-gpu_resize-v1_size-v1_aug_reg-nonrigid_20231116-134048"
+    ddf_directory = f"{base}/{ckpt}"
+    dataset_name = "2022-04-18-04"
+    registration_problem = "1012to1453"
+    #ddf_array, warped_moving_image = warp_channel_images(
+    #        ddf_directory, dataset_name, registration_problem)
+    #print(ddf_array.shape)
+    #print(warped_moving_image.shape)
+    warp_image_roi(ddf_directory,
+                   dataset_name,
+                   registration_problem)
+
--- wormalign/warp.py
+++ wormalign/warp.py
@@ -1,8 +1,8 @@
 from euler_gpu.preprocess import initialize
 from euler_gpu.transform import transform_image_3d
 from numpy.typing import NDArray
-from wormalign.utils import (locate_dataset, get_cropped_image, 
-    get_image_T, get_image_CM)
+from typing import Dict, List, Tuple
+from wormalign.utils import (locate_dataset, get_cropped_image, get_image_T)
 import deepreg.model.layer as layer
 import json
 import nibabel as nib
@@ -11,191 +11,211 @@ import tensorflow as tf
 import torch
 
 
-with open("resources/registration_problems.json", "r") as f:
-    ALL_PROBLEMS = json.load(f)
+# Load JSON resources
+def load_json(file_path):
+    with open(file_path, "r") as f:
+        return json.load(f)
 
-with open("resources/center_of_mass.json", "r") as f:
-    CM_DICT = json.load(f)
+ALL_PROBLEMS = load_json("resources/registration_problems.json")
+CM_DICT = load_json("resources/center_of_mass.json")
+EULER_PARAMETERS_DICT = load_json("resources/euler_parameters.json")
+PAIR_NUM_DICT = load_json("resources/problem_to_pairnum.json")
 
-with open("resources/euler_parameters.json", "r") as f:
-    EULER_PARAMETERS_DICT = json.load(f)
+class ImageWarper:
 
-with open(f"resources/problem_to_pairnum.json", "r") as f:
-    PAIR_NUM_DICT = json.load(f)
+    def __init__(
+        self,
+        ddf_directory: str,
+        dataset_name: str,
+        registration_problem: str,
+        image_shape: Tuple[int, int, int],
+        device_name: str = "cuda:0"
+    ):
+        self.ddf_directory = ddf_directory
+        self.dataset_name = dataset_name
+        self.registration_problem = registration_problem
+        self.problem_id = f"{dataset_name}/{self.registration_problem}"
+        self.image_shape = image_shape
+        self.device = torch.device(self.device_name)
 
-def warp_channel_image(ddf_directory: str,
-                       dataset_name: str,
-                       registration_problem: str) -> NDArray[np.float_]:
+    def get_network_outputs(
+        self,
+        target: str = "all"
+    ):
+        network_output_path = self._get_problem_path(
+                self.registration_problem)
+        network_outputs = self._load_images(network_outputs_path)
 
-    problem_id = f"{dataset_name}/{registration_problem}"
-    pair_num = PAIR_NUM_DICT[problem_id]
-    #pair_num = get_pair_num(dataset_name, registration_problem)
-    network_outputs_path = f"{ddf_directory}/test/pair_{pair_num}"
-    ddf_nii_path = f"{network_outputs_path}/ddf.nii.gz"
-    ddf_array = nib.load(ddf_nii_path).get_fdata()
-    warped_moving_image_nii_path = \
-            f"{network_outputs_path}/pred_fixed_image.nii.gz"
-    warped_moving_image = nib.load(warped_moving_image_nii_path).get_fdata()
+        if target == "all":
+            return network_outputs
+        elif:
+            return network_outputs[target]
 
-    fixed_image_nii_path = \
-            f"{network_outputs_path}/fixed_image.nii.gz"
-    fixed_image = nib.load(fixed_image_nii_path).get_fdata()
+    def _get_problem_path(self):
+        pair_num = PAIR_NUM_DICT[self.problem_id]
+        return f"{self.ddf_directory}/test/pair_{pair_num}"
 
-    moving_image_nii_path = \
-            f"{network_outputs_path}/moving_image.nii.gz"
-    moving_image = nib.load(moving_image_nii_path).get_fdata()
+    def _load_images(
+        self,
+        network_outputs_path: str
+    ):
+        ddf_nii_path = f"{network_outputs_path}/ddf.nii.gz"
+        ddf_array = nib.load(ddf_nii_path).get_fdata()
+        warped_moving_image = nib.load(
+                f"{network_outputs_path}/pred_fixed_image.nii.gz").get_fdata()
+        fixed_image = nib.load(
+                f"{network_outputs_path}/fixed_image.nii.gz").get_fdata()
+        moving_image = nib.load(
+                f"{network_outputs_path}/moving_image.nii.gz").get_fdata()
 
-    return ddf_array, fixed_image, moving_image, warped_moving_image
+        return {
+                "ddf": ddf_array,
+                "warped_moving_image": warped_moving_image,
+                "fixed_image": fixed_image,
+                "moving_image": moving_image
+        }
 
+    def _preprocess_image_roi(self):
+        """
+        Resize the ROI images and Euler-transform them with the same parameters
+        of their corresponding channel images.
+        """
+        nrrd_images_path = locate_dataset(self.dataset_name)
+        t_moving, t_fixed = self.problem_id.split('to')
+        fixed_image_roi_path = f"{nrrd_images_path}/img_roi_watershed/{t_fixed}.nrrd"
+        moving_image_roi_path = f"{nrrd_images_path}/img_roi_watershed/{t_moving}.nrrd"
 
-def warp_image_roi(ddf_directory: str,
-                   dataset_name: str,
-                   registration_problem: str) -> NDArray[np.float_]:
-
-    problem_id = f"{dataset_name}/{registration_problem}"
-    pair_num = PAIR_NUM_DICT[problem_id]
-    #pair_num = get_pair_num(dataset_name, registration_problem)
-    network_outputs_path = f"{ddf_directory}/test/pair_{pair_num}"
-    ddf_nii_path = f"{network_outputs_path}/ddf.nii.gz"
-    ddf_array = nib.load(ddf_nii_path).get_fdata()
-
-    nrrd_images_path = locate_dataset(dataset_name)
-    t_moving, t_fixed = registration_problem.split('to')
-    fixed_image_roi_path = \
-            f"{nrrd_images_path}/img_roi_watershed/{t_fixed}.nrrd"
-    moving_image_roi_path = \
-            f"{nrrd_images_path}/img_roi_watershed/{t_moving}.nrrd"
-
-    resized_fixed_image_roi = resize_image_roi(
+        # resize the fixed and moving image ROIs
+        resized_fixed_image_roi = self._resize_image_roi(
                 fixed_image_roi_path,
-                CM_DICT[problem_id][1])
-    resized_moving_image_roi = resize_image_roi(
+                CM_DICT[problem_id][1]
+        )
+        resized_moving_image_roi = self._resize_image_roi(
                 moving_image_roi_path,
-                CM_DICT[problem_id][0])
-    device_name = 0
-    # warp the moving image roi with euler parameters
-    euler_warped_moving_image_roi = euler_transform_image_roi(
-                resized_moving_image_roi,
-                problem_id,
-                device_name)
-    # warp the moving image roi with ddf
-    resized_moving_image_roi_tf = tf.cast(
-                tf.expand_dims(euler_warped_moving_image_roi,
-                axis=0), dtype=tf.float32
-    )
-    warping = layer.Warping(fixed_image_size=resized_moving_image_roi_tf.shape[1:4],
-                            interpolation="nearest")
-    warped_moving_image_roi_tf = warping(inputs=[ddf_array,
-                resized_moving_image_roi_tf])
-    warped_moving_image_roi = warped_moving_image_roi_tf.numpy()[0]
+                CM_DICT[problem_id][0]
+        )
+        euler_transformed_moving_image_roi = self._euler_transform_image_roi(
+                resized_moving_image_roi
+        )
+        return {
+            "fixed_image_roi": resized_fixed_image_roi,
+            "moving_image_roi": resized_fixed_image_roi,
+            "euler_tfmed_moving_image_roi": euler_transformed_moving_image_roi
+        }
 
-    return resized_fixed_image_roi, resized_moving_image_roi, euler_warped_moving_image_roi, warped_moving_image_roi
+    def _resize_image_roi(
+        self,
+        image_roi_path: str,
+        image_CM: List[int, int, int]
+    ):
+        image_roi_T = get_image_T(image_roi_path)
 
-'''
-def get_pair_num(dataset_name: str, registration_problem: str):
+        return get_cropped_image(
+                image_roi_T,
+                image_CM,
+                self.image_shape,
+                -1).astype(np.float32)
 
-    problems = ALL_PROBLEMS["test"][dataset_name]
-
-    return problems.index(registration_problem)
-'''
-
-def resize_image_roi(image_roi_path: str, image_CM: list[int]):
-
-    image_roi_T = get_image_T(image_roi_path)
-    resized_image_roi = get_cropped_image(
-        image_roi_T, image_CM, (208, 96, 56), -1).astype(np.float32)
-
-    return resized_image_roi
-
-
-def euler_transform_image_roi(moving_image_roi: NDArray[np.int_],
-                problem_id: str,
-                device_name: int) -> NDArray[np.float_]:
-
-    device = torch.device(f"cuda:{device_name}")
-    x_dim, y_dim, z_dim = moving_image_roi.shape
-
-    _memory_dict_xy = initialize(
-            np.zeros((x_dim, y_dim)).astype(np.float32),
-            np.zeros((x_dim, y_dim)).astype(np.float32),
-            np.zeros(z_dim),
-            np.zeros(z_dim),
-            np.zeros(z_dim),
-            z_dim,
-            device
-    )
-    _memory_dict_xz = initialize(
-            np.zeros((x_dim, z_dim)).astype(np.float32),
-            np.zeros((x_dim, z_dim)).astype(np.float32),
-            np.zeros(y_dim),
-            np.zeros(y_dim),
-            np.zeros(y_dim),
-            y_dim,
-            device
-    )
-    _memory_dict_yz = initialize(
-            np.zeros((y_dim, z_dim)).astype(np.float32),
-            np.zeros((y_dim, z_dim)).astype(np.float32),
-            np.zeros(x_dim),
-            np.zeros(x_dim),
-            np.zeros(x_dim),
-            x_dim,
-            device
-    )
-    best_transformation_xy = torch.tensor(
-                EULER_PARAMETERS_DICT[problem_id]["xy"]).to(device)
-    transformed_moving_image_roi_xyz = transform_image_3d(
+    def _euler_transform_image_roi(
+        self,
+        moving_image_roi: NDArray[np.int_]
+    ):
+        x_dim, y_dim, z_dim = self.image_shape
+        _memory_dict_xy = self._initialize_memory_dict(x_dim, y_dim, z_dim)
+        return self._apply_euler_parameters(
                 moving_image_roi,
-                _memory_dict_xy,
-                best_transformation_xy,
-                device
-    )
-    best_transformation_xz = torch.tensor(
-                EULER_PARAMETERS_DICT[problem_id]["xz"]).to(device)
-    transformed_moving_image_roi_xzy = transform_image_3d(
-                np.transpose(transformed_moving_image_roi_xyz, (0, 2, 1)),
-                _memory_dict_xz,
-                best_transformation_xz,
-                device
-    )
-    best_transformation_yz = torch.tensor(
-                EULER_PARAMETERS_DICT[problem_id]["yz"]).to(device)
-    transformed_moving_image_roi_yzx = transform_image_3d(
-                np.transpose(transformed_moving_image_roi_xzy, (2, 1, 0)),
-                _memory_dict_yz,
-                best_transformation_yz,
-                device
-    )
-    dz = EULER_PARAMETERS_DICT[problem_id]["dz"]
-    warped_moving_image_roi = np.transpose(transformed_moving_image_roi_yzx, (2, 0, 1))
+                _memory_dict_xy
+        )
 
-    final_moving_image_roi_xyz = np.full(
-            warped_moving_image_roi.shape, 0)
-    # translate along the z-axis
-    if dz < 0:
-        final_moving_image_roi_xyz[:, :, :dz] = \
-            warped_moving_image_roi[:, :, -dz:]
-    elif dz > 0:
-        final_moving_image_roi_xyz[:, :, dz:] = \
-            warped_moving_image_roi[:, :, :-dz]
-    elif dz == 0:
-        final_moving_image_roi_xyz = warped_moving_image_roi
+    def _initialize_memory_dict(
+        self,
+        dim_1: int,
+        dim_2: int,
+        dim_3: int
+    ) -> Dict[str, torch.Tensor]:
+        _memory_dict = initialize(
+                np.zeros((dim_1, dim_2)).astype(np.float32),
+                np.zeros((dim_1, dim_2)).astype(np.float32),
+                np.zeros(dim_3),
+                np.zeros(dim_3),
+                np.zeros(dim_3),
+                dim_3,
+                self.device
+        )
+        return _memory_dict
 
-    return final_moving_image_roi_xyz
+    def _apply_euler_parameters(
+        self,
+        moving_image_roi: NDArray[np.int_],
+        memory_dict: Dict[str, torch.Tensor],
+        dimension: str = "xy"
+    ) ->  NDArray[np.int_]:
 
+        best_transformation = torch.tensor(
+            EULER_PARAMETERS_DICT[self.problem_id][dimension]
+        ).to(self.device)
 
-if __name__ == "__main__":
+        moving_image_roi = self._adjust_image_shape(
+                moving_image_roi,
+                dimension
+        )
+        transformed_moving_image_roi = transform_image_3d(
+                moving_image_roi,
+                memory_dict,
+                best_transformation,
+                self.device
+        )
+        translated_moving_image_roi = self._translate_image(
+                self._adjust_image_shape(
+                    transformed_moving_image_roi,
+                    "xy"
+                )
+        )
 
-    base = "/home/alicia/data_personal/regnet_ckpt/logs_predict"
-    ckpt = "euler-gpu_resize-v1_size-v1_aug_reg-nonrigid_20231116-134048"
-    ddf_directory = f"{base}/{ckpt}"
-    dataset_name = "2022-04-18-04"
-    registration_problem = "1012to1453"
-    #ddf_array, warped_moving_image = warp_channel_images(
-    #        ddf_directory, dataset_name, registration_problem)
-    #print(ddf_array.shape)
-    #print(warped_moving_image.shape)
-    warp_image_roi(ddf_directory,
-                   dataset_name,
-                   registration_problem)
+        return translated_moving_image_roi
+
+    def _adjust_image_shape(
+        self,
+        image: NDArray[np.int_],
+        dimension: str,
+    ) -> NDArray[np.int_]:
+
+        x_dim, y_dim, z_dim = self.image_shape
+        reshaping_dict = {
+            (y_dim, z_dim, x_dim): {
+                "xy": (2, 0, 1),
+                "xz": (2, 1, 0),
+                "yz": (0, 1, 2)
+            },
+            (x_dim, z_dim, y_dim): {
+                "xy": (0, 2, 1),
+                "yz": (2, 1, 0),
+                "xz": (0, 1, 2)
+            },
+            (x_dim, y_dim, z_dim): {
+                "yz": (1, 2, 0),
+                "xz": (0, 2, 1),
+                "xy": (0, 1, 2)
+            }
+        }
+        order = reshaping_dict[image.shape][dimension]
+
+        return np.transpose(image, order)
+
+    def _translate_image(
+        self,
+        image: NDArray[np.int_]
+    ) -> NDArray[np.int_]:
+
+        dz = EULER_PARAMETERS_DICT[self.problem_id]["dz"]
+        translated_image = np.full(self.image_shape, 0)
+
+        if dz < 0:
+            translated_image[:, :, :dz] = image[:, :, -dz:]
+        elif dz > 0:
+            translated_image[:, :, dz:] = image[:, :, :-dz]
+        elif dz == 0:
+            translated_image = image
+
+    return translated_image
 
