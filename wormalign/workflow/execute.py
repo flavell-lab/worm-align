6900f946b6f91c9cd8502ea1195cfba0eef7c285
Alicia Lu <lualicia88@gmail.com>
2024-01-04 11:13:09 -0500

    add argparser

--- wormalign/workflow/execute.py
+++ wormalign/workflow/execute.py
@@ -66,4 +66,28 @@ def train(
    )"""
    pass
 
+def main():
 
+    parser = argparse.ArgumentParser(description="preprocessing pipeline")
+    parser.add_argument("-p", "--path", type=str, nargs=1,
+                metavar="path_to_save_datasets",
+                help="Path where to keep the preprocessed datasets")
+    parser.add_argument("-s", "--shape", type=int, nargs=3, 
+                metavar=("xdim", "ydim", "zdim"),
+                help="Shape of the target image in the order of x-y-z")
+    parser.add_argument("-b", "--batchsize", type=int, nargs=1,
+                metavar="batch_size",
+                help="Batch size used for searching Euler parameters")
+    parser.add_argument("-d", "--device", type=str, nargs=1,
+                metavar="device_name",
+                help="CUDA device name")
+    parser.add_argument("-train", "--train_datasets", type=str, nargs="+",
+                metavar="datasets for training",
+                help="enter names of training datasets")
+    parser.add_argument("-valid", "--valid_datasets", type=str, nargs="+",
+                metavar="datasets for validation",
+                help="enter names of validation datasets")
+    parser.add_argument("-test", "--test_datasets", type=str, nargs="+",
+                metavar="datasets for testing",
+                help="enter names of test datasets")
+    args = parser.parse_args()
