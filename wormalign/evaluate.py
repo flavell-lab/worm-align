65f5e3aaf7ce3b3839595f185130f0f90cbd970c
Alicia Lu <lualicia88@gmail.com>
2024-01-29 09:34:03 -0500

    bug fixed

--- wormalign/evaluate.py
+++ wormalign/evaluate.py
@@ -5,83 +5,105 @@ from wormalign.benchmark import (CentroidDistScore,
         NonRigidPenalty)
 from wormalign.utils import write_to_json
 from wormalign.warp import ImageWarper
+from wormalign.warp_label import LabelWarper
+import glob
 import numpy as np
+import h5py
+import tensorflow as tf
+
 
 def get_all_scores(test_dataset_path, network_output_path, target_image_shape):
 
-    warper = ImageWarper(
+    image_warper = ImageWarper(
             network_output_path,
-            "2022-06-14-01", # `dataset_name` to be set later
-            "1000to1001", # `registration_problem` to be set later
+            None, # `dataset_name` to be set later
+            None, # `registration_problem` to be set later
             target_image_shape
     )
-    # test_dataset_path:= "/data3/prj_regsiter/test/nonaugmented"
-
-    all_dataset_paths = glob.glob(f"{test_dataset_path}/*")
+    label_warper = LabelWarper(
+            None,
+            None,
+            target_image_shape,
+    )
+    label_warper.ddf_directory = network_output_path
+    #all_dataset_paths = glob.glob(f"{test_dataset_path}/*")
+    all_dataset_paths = [test_dataset_path]
 
     centroid_distance = dict()
     image_lncc = dict()
     image_gncc = dict()
     nonrigid_penalty = dict()
 
-    def compute_score(score_class, fixed_, moving_):
-
-        return score_class().call(fixed_, moving_).numpy()[0]
+    def compute_score(score_class, *args):
+        if len(args) == 2:
+            return score_class().call(args[0], args[1]).numpy()[0]
+        elif len(args) == 1:
+            return score_class(
+                    img_size=target_image_shape
+            ).call(args[0]).numpy()[0]
 
     def get_scores_for_one_dataset(dataset_path):
 
         problems = list(h5py.File(f"{dataset_path}/fixed_images.h5").keys())
-        dataset_name = dataset_path.split("/")[-1]
-        warper.dataset_name = dataset_name
+        dataset_name = dataset_path.split("/")[-1].split("_")[0]
+        image_warper.dataset_name = dataset_name
+        label_warper.dataset_name = dataset_name
 
         centroid_distance[dataset_name] = dict()
         image_lncc[dataset_name] = dict()
         image_gncc[dataset_name] = dict()
         nonrigid_penalty[dataset_name] = dict()
 
-        for problem in problems[:1]:
+        for problem in tqdm(problems):
 
-            warper.registration_problem = problem
-            output_image_dict = warper.get_network_outputs()
-            output_roi_dict = warper.get_image_roi()
+            image_warper.registration_problem = problem
+            label_warper.registration_problem = problem
+            # need to manually update `LabelWarper.pair_num`
+            label_warper.pair_num = label_warper.pairnum_dict[label_warper.problem_id]
+
+            output_image_dict = image_warper.get_network_outputs()
+            output_roi_label_dict = label_warper.get_image_roi()
 
             ddf = reformat(output_image_dict["ddf"])
             warped_moving_image = reformat(output_image_dict["warped_moving_image"])
             fixed_image = reformat(output_image_dict["fixed_image"])
             moving_image = reformat(output_image_dict["moving_image"])
 
+            """
             warped_moving_centroids = reformat(
                 compute_centroids_3d(
-                    output_roi_dict["warped_moving_image_roi"]
+                    output_roi_label_dict["warped_moving_image_roi"], 200
                 )
             )
             fixed_centroids = reformat(
                 compute_centroids_3d(
-                    output_roi_dict["fixed_image_roi"]
+                    output_roi_label_dict["fixed_image_roi"], 200
                 )
             )
             moving_centroids = reformat(
                 compute_centroids_3d(
-                    output_roi_dict["moving_image_roi"]
+                    output_roi_label_dict["moving_image_roi"], 200
                 )
             )
             centroid_distance[dataset_name][problem] = compute_score(
                     CentroidDistScore, fixed_centroids, moving_centroids)
+            """
             image_lncc[dataset_name][problem] = compute_score(
-                    LocalNormalizedCrossCorrelation, fixed_image, moving_image)
+                    LocalNormalizedCrossCorrelation, fixed_image, warped_moving_image)
+
             image_gncc[dataset_name][problem] = compute_score(
-                    GlobalNormalizedCrossCorrelation, fixed_image, moving_image)
+                    GlobalNormalizedCrossCorrelation, fixed_image, warped_moving_image)
+            """
             nonrigid_penalty[dataset_name][problem] = compute_score(
                     NonRigidPenalty, ddf)
-
-    for dataset_path in tqdm(all_dataset_paths):
+            """
+    for dataset_path in all_dataset_paths:
         get_scores_for_one_dataset(dataset_path)
 
-    write_to_json(centroid_distance, "centroid_distance", "scores")
+    #write_to_json(centroid_distance, "centroid_distance_A1", "scores")
     write_to_json(image_lncc, "image_lncc", "scores")
     write_to_json(image_gncc, "image_gncc", "scores")
-    write_to_json(nonrigid_penalty, "nonrigid_penalty", "scores")
-
+    #write_to_json(nonrigid_penalty, "nonrigid_penalty_ALv1", "scores")
 
 def reformat(numpy_array):
     return tf.convert_to_tensor(numpy_array[np.newaxis, ...].astype(np.float32))
